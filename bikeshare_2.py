8852aedf27ae7150505abcd19441b9c3cdfc3cd2
Rustam <daurust@outlook.com>
2024-08-09 12:10:49 +0200

    refactor: Add while loop for city  input

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -17,7 +17,12 @@ def get_filters():
     """
     print('Hello! Let\'s explore some US bikeshare data!')
     # get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
-
+    while True:
+        city = input("Which city would you like to explore? (chicago, new york city, washington): ").lower()
+        if city in CITY_DATA.keys():
+            break
+        else:
+            print("Invalid input. Please choose from chicago, new york city, or washington.")
 
     # get user input for month (all, january, february, ... , june)
 
